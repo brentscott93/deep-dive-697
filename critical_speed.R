74bce1ef012ded63db2ee2222b9a224fac6ea82e
brentscott93 <bdscott@umass.edu>
2021-12-09 12:40:16 -0500

    checkpoint- replicate 1b

--- critical_speed.R
+++ critical_speed.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(here)
+library(cowplot)
+library(RColorBrewer)
 
 # copy pasta from Kirby 2021 paper
 runner_data <- 
@@ -19,53 +21,130 @@ runner_data <-
     12, "Birhanu Balew", NA, NA, NA, 13:43.3, "00:10.50"
   ) 
 
-calc_d_left <- function(d_cs, t, d_prior){
-  (d_cs * t) + d_prior
-}
 
 # Estimated lap speeds from digitized graph
 race_data <- 
   read_csv(here("kirby-fig1a.csv")) %>% 
   mutate(split = 1:nrow(.),
-         lap_split_per_400m = round(400/race_speed_meters_second, 0),
-         relative_lap_timer =  map(lap_split_per_400m, ~seq(1, .x, by = 1)))
+         lap_distance= c(rep(400, 11), 200, 400),
+         total_distance = cumsum(lap_distance),
+         lap_split = lap_distance/lap_leader_speed_meters_second,
+         relative_lap_timer =  map(lap_split, ~seq(1, .x, by = 1)))
 
 
-
-d_prior <- as.list(runner_data$d_prime_meters)
-race_data$d_left_data <- list(tibble(athlete = runner_data$athlete,
-                                   d_left = list(NA)))
-# d_bal  <- list()
-# final_d <- list()
-for(runner in seq_len(nrow(runner_data))){
-  # get critical speed and D' for current runner
-  cs <- runner_data$critical_speed_meters_second[[runner]]
-  d_prime <- runner_data$d_prime_meters[[runner]]
-  
-  for(lap in seq_len(nrow(race_data))){
-    # get current lap speed in m/s and calculate D_cs
-    lap_speed <- race_data$race_speed_meters_second[[lap]]
-    d_cs <- cs - lap_speed
+calc_d_balance <- function(runner_data, race_data){
+  #d_start_of_lap is d_prior
+  d_start_of_lap <- as.list(runner_data$d_prime_meters)
+  d_balance_data <- vector("list", nrow(runner_data))
+  # d_bal  <- list()
+  # final_d <- list()
+  for(runner in seq_len(nrow(runner_data))){
+    # get critical speed and D' for current runner
+    cs <- runner_data$critical_speed_meters_second[[runner]]
+    d_prime <- runner_data$d_prime_meters[[runner]]
     
-    # Calculate D_exp, or d_left
-    d_left <- calc_d_left(d_cs = d_cs, 
-                          t = race_data$relative_lap_timer[[lap]],
-                          d_prior = d_prior[[runner]][[lap]])
+    if(is.na(cs)){ next }
     
-    d_prior[[runner]][[lap+1]] <- tail(d_left, 1)
+    runner_d_balance <- list()
+    for(lap in seq_len(nrow(race_data))){
+      # get current lap speed in m/s and calculate D_cs
+      lap_speed <- race_data$lap_leader_speed_meters_second[[lap]]
+     
+      lap_time <- race_data$relative_lap_timer[[lap]]
+      lap_distance <- race_data$lap_distance[[lap]]
+      d_cs <- cs - lap_speed
+      
+      if(lap_speed > cs){
+      # Calculate d_drain (kirby calls this d_exp for expended)
+      t <- race_data$relative_lap_timer[[lap]] 
+      d_sol <- d_start_of_lap[[runner]][[lap]]
+      d_drain <- ( d_cs * t ) + d_sol
+      
+      d_start_of_lap[[runner]][[lap+1]] <- tail(d_drain, 1)
+      
+      d_balance <- d_drain
+      balance_status <- "drain"
+      
+      } else if(lap_speed<= cs){
+      
+      d_expended <- d_prime - d_start_of_lap[[runner]][[lap]]
+      d_gain <- d_prime - d_expended * exp((-d_cs*lap_time)/d_prime)
+      
+      d_start_of_lap[[runner]][[lap+1]] <- tail(d_gain, 1)
+      
+      d_balance <- d_gain
+      balance_status <- "gain"
+      
+      }
+      
+      
+      runner_lap_data <- tibble(athlete = runner_data$athlete[[runner]],
+                                lap = lap,
+                                lap_time = lap_time,
+                                meters_second = lap_speed,
+                                d_balance = d_balance,
+                                balance_status = balance_status)
+      
+      runner_d_balance[[lap]] <- runner_lap_data
+      
+    }
     
-    race_data$d_left_data[[lap]]$d_left[[runner]] <- d_left
-    
-    # d_balance <- d_prime - d_expended*exp(-d_cs[[i]]^rel_lap_times[[i]]/d_prime)
-    # d_bal[[i]] <- d_balance
-    # if(cs <= lap_speed[[i]]){
-    #   final_d[[i]] <- d_expended
-    # } else {
-    #   final_d[[i]] <- d_balance
-    # }
+  d_balance_data[[runner]] <- 
+    do.call("rbind", runner_d_balance) %>% 
+    mutate(total_race_time = seq_len(nrow(.)),
+           total_race_distance_m = cumsum(meters_second))
   }
+  
+  d_balance_data <- do.call("rbind", d_balance_data)
 }
 
 
 
+
+
+
+
+
+runner1 <- filter(d_balance_data, athlete %in% c("Muktar Edris", "Mohammed Farah", "Paul Chelimo", "Yomif Kejelcha")) %>% 
+  mutate(source = "Replication")
+
+
+
+kirby_d_balance <- 
+  read_csv(here("kirby-fig1b.csv")) %>% 
+  pivot_longer(cols = !x, names_to = "last_name", values_to = "d_balance") %>% 
+  mutate(athlete = case_when(
+                    last_name == "Edris" ~ "Muktar Edris",
+                    last_name == "Farah" ~ "Mohammed Farah",
+                    last_name == "Chelimo" ~ "Paul Chelimo",
+                    last_name == "Kejelcha" ~ "Yomif Kejelcha"
+                    ),
+  total_race_distance_m = x,
+  source = "Digitized Original")
+
+run <- bind_rows(runner1, kirby_d_balance)
+
+run$athlete <- factor(run$athlete, levels = c("Muktar Edris",
+                                              "Mohammed Farah",
+                                              "Paul Chelimo",
+                                              "Yomif Kejelcha"))
+
+ggplot()+
+  geom_line(data = run, 
+            aes(total_race_distance_m, 
+                d_balance, 
+                color = athlete, 
+                linetype = source,
+                size = source
+                ))+
+  scale_size_manual(values = c(0.5, 1))+
+  ylab("D' Balance")+
+  xlab("Race Distance (m)")+
+  ggtitle("Replication of Kirby 2021 Figure 1B")+
+  scale_color_manual(values = brewer.pal(4, "Dark2"))+
+  theme_cowplot()
+
+
+ggplot(kirby_d_balance)+
+  geom_line(aes(x, d_balance))
  
